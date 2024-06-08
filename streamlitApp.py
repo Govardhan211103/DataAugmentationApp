2fe063b7df5372193d537a96ebc418efed9dd2ab
Govardhan211103 <vgovardhanvarma.vh@gmail.com>
2024-06-08 23:22:08 +0530

    secrets added

--- streamlitApp.py
+++ streamlitApp.py
@@ -268,7 +268,6 @@ def main():
     st.markdown("""
         <style>
                 .block-container {
-                    padding-top: 1rem;
                     padding-bottom: 0.5rem;
                     padding-left: 2rem;
                     padding-right: 2rem;
